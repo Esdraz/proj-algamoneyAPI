7a23c1a4c5b33d81c78f59bb12f88164b71e0715
Francisco <fsousa.jpa@gmail.com>
2022-08-18 16:30:53 -0300

    add methods (listar, criar and buscar) on CategoriaResource

--- src/main/java/com/esdraz/algamoneyapi/resource/CategoriaResource.java
+++ src/main/java/com/esdraz/algamoneyapi/resource/CategoriaResource.java
@@ -3,11 +3,15 @@ package com.esdraz.algamoneyapi.resource;
 import com.esdraz.algamoneyapi.model.Categoria;
 import com.esdraz.algamoneyapi.repository.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/categorias")
@@ -19,5 +23,22 @@ public class CategoriaResource {
     @GetMapping
     public List<Categoria> listar() {
         return categoriaRepository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+        Categoria categoriaSalva = categoriaRepository.save(categoria);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
+                .buildAndExpand(categoriaSalva.getId()).toUri();
+        response.setHeader("Location", uri.toASCIIString());
+
+        return ResponseEntity.created(uri).body(categoriaSalva);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Categoria> buscarPelaId(@PathVariable Long id) {
+        Optional<Categoria> categoria = this.categoriaRepository.findById(id);
+        return categoria.isPresent() ? ResponseEntity.ok(categoria.get()) : ResponseEntity.notFound().build();
     }
 }
