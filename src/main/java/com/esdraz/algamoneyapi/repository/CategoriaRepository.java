0d7b6bf6c0384b3f93e1a766d3903962ac8ec8d5
Francisco <fsousa.jpa@gmail.com>
2022-08-18 13:22:57 -0300

    add model, repository and resource Classes

---
+++ src/main/java/com/esdraz/algamoneyapi/repository/CategoriaRepository.java
@@ -0,0 +1,8 @@
+package com.esdraz.algamoneyapi.repository;
+
+import com.esdraz.algamoneyapi.model.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+}
