44421466cb1ae64a8d0d6c871dcbc7c18a316702
Francisco <fsousa.jpa@gmail.com>
2022-08-25 20:47:29 -0300

    feat: add parcial update to value 'ativo' in pessoa

--- src/main/java/com/esdraz/algamoneyapi/service/PessoaService.java
+++ src/main/java/com/esdraz/algamoneyapi/service/PessoaService.java
@@ -14,8 +14,19 @@ public class PessoaService {
     private PessoaRepository pessoaRepository;
 
     public Pessoa atualizar(Long id, Pessoa pessoa) {
-        Pessoa pessoaSalva = this.pessoaRepository.findById(id).orElseThrow(() -> new EmptyResultDataAccessException(1));
+        Pessoa pessoaSalva = buscarPessoaPeloId(id);
         BeanUtils.copyProperties(pessoa, pessoaSalva, "id");
         return pessoaRepository.save(pessoaSalva);
+    }
+
+    public void atualizarPropriedadeAtivo(Long id, Boolean ativo) {
+        Pessoa pessoaSalva = buscarPessoaPeloId(id);
+        pessoaSalva.setAtivo(ativo);
+        pessoaRepository.save(pessoaSalva);
+    }
+
+    private Pessoa buscarPessoaPeloId(Long id) {
+        Pessoa pessoaSalva = this.pessoaRepository.findById(id).orElseThrow(() -> new EmptyResultDataAccessException(1));
+        return pessoaSalva;
     }
 }
