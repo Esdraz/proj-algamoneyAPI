6d10d1d5dbdf69b2fdd24d8beec8ac4c343ca17b
Francisco <fsousa.jpa@gmail.com>
2022-08-25 13:38:12 -0300

    refactor: header location now is controlled by event classes

--- src/main/java/com/esdraz/algamoneyapi/resource/CategoriaResource.java
+++ src/main/java/com/esdraz/algamoneyapi/resource/CategoriaResource.java
@@ -1,16 +1,16 @@
 package com.esdraz.algamoneyapi.resource;
 
+import com.esdraz.algamoneyapi.event.RecursoCriadoEvent;
 import com.esdraz.algamoneyapi.model.Categoria;
 import com.esdraz.algamoneyapi.repository.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
-import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,6 +21,9 @@ public class CategoriaResource {
     @Autowired
     private CategoriaRepository categoriaRepository;
 
+    @Autowired
+    private ApplicationEventPublisher publisher;
+
     @GetMapping
     public List<Categoria> listar() {
         return categoriaRepository.findAll();
@@ -29,12 +32,8 @@ public class CategoriaResource {
     @PostMapping
     public ResponseEntity<Categoria> criar(@Valid @RequestBody Categoria categoria, HttpServletResponse response) {
         Categoria categoriaSalva = categoriaRepository.save(categoria);
-
-        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
-                .buildAndExpand(categoriaSalva.getId()).toUri();
-        response.setHeader("Location", uri.toASCIIString());
-
-        return ResponseEntity.created(uri).body(categoriaSalva);
+        publisher.publishEvent(new RecursoCriadoEvent(this, response, categoriaSalva.getId()));
+        return ResponseEntity.status(HttpStatus.CREATED).body(categoriaSalva);
     }
 
     @GetMapping("/{id}")
--- src/main/java/com/esdraz/algamoneyapi/resource/PessoaResource.java
+++ src/main/java/com/esdraz/algamoneyapi/resource/PessoaResource.java
@@ -1,8 +1,11 @@
 package com.esdraz.algamoneyapi.resource;
 
+import com.esdraz.algamoneyapi.event.RecursoCriadoEvent;
 import com.esdraz.algamoneyapi.model.Pessoa;
 import com.esdraz.algamoneyapi.repository.PessoaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -20,6 +23,9 @@ public class PessoaResource {
     @Autowired
     private PessoaRepository pessoaRepository;
 
+    @Autowired
+    private ApplicationEventPublisher publisher;
+
     @GetMapping
     public List<Pessoa> listar() {
         return pessoaRepository.findAll();
@@ -28,12 +34,8 @@ public class PessoaResource {
     @PostMapping
     public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
         Pessoa pessoaSalva = pessoaRepository.save(pessoa);
-
-        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}")
-                .buildAndExpand(pessoaSalva.getId()).toUri();
-        response.setHeader("Location", uri.toASCIIString());
-
-        return ResponseEntity.created(uri).body(pessoaSalva);
+        publisher.publishEvent(new RecursoCriadoEvent(this, response, pessoaSalva.getId()));
+        return ResponseEntity.status(HttpStatus.CREATED).body(pessoaSalva);
     }
 
     @GetMapping("/{id}")
