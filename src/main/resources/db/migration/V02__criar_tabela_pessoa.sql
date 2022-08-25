e44922d3475bd7cf959dc2e967c5178c974e33f0
Francisco <fsousa.jpa@gmail.com>
2022-08-24 21:16:33 -0300

    add test inserts on migration 02

--- src/main/resources/db/migration/V02__criar_tabela_pessoa.sql
+++ src/main/resources/db/migration/V02__criar_tabela_pessoa.sql
@@ -13,4 +13,28 @@ CREATE TABLE pessoa (
 
 INSERT INTO pessoa
 (nome, logradouro, numero, complemento, bairro, cep, cidade, estado, ativo)
-VALUES('Francisco', 'rua Severina Maria Vasconcelos de Carvalho', 136, 'casa', 'cuiá', 58077156, 'João Pessoa', 'Paraíba', true);
+VALUES('Francisco Sousa', 'rua Severina Maria Vasconcelos de Carvalho', 200, 'casa', 'geisel', 58777657, 'João Pessoa', 'Paraíba', true);
+
+INSERT INTO pessoa
+(nome, logradouro, numero, complemento, bairro, cep, cidade, estado, ativo)
+VALUES('Pedro ', 'rua dali', 12, 'ap 200', 'tamandua', 23567896, 'Fortaleza', 'Ceara', true);
+
+INSERT INTO pessoa
+(nome, logradouro, numero, complemento, bairro, cep, cidade, estado, ativo)
+VALUES('Marcia', 'rua sem saida', 123, 'casa', 'flores', 58777334, 'Patos', 'Paraíba', true);
+
+INSERT INTO pessoa
+(nome, logradouro, numero, complemento, bairro, cep, cidade, estado, ativo)
+VALUES('Paulo Cezar', 'av hilton solto', 234, 'casa', 'cristo', 58777657, 'João Pessoa', 'Paraíba', true);
+
+INSERT INTO pessoa
+(nome, logradouro, numero, complemento, bairro, cep, cidade, estado, ativo)
+VALUES('Maria Fernanda', 'av paulista', 111, 'casa', 'Estados', 58677657, 'João Pessoa', 'Paraíba', true);
+
+INSERT INTO pessoa
+(nome, logradouro, numero, complemento, bairro, cep, cidade, estado, ativo)
+VALUES('Amanda', 'praça da independencia', 678, 'casa', 'Centro', 58737657, 'João Pessoa', 'Paraíba', true);
+
+INSERT INTO pessoa
+(nome, logradouro, numero, complemento, bairro, cep, cidade, estado, ativo)
+VALUES('Jessica', 'av guararapes', 667, 'apt 22', 'Miramar', 53377678, 'João Pessoa', 'Paraíba', true);
